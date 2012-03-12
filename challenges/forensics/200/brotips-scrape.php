28ac723b3ee43736323e4b2f7007e0364cd15b00
Nick Soracco <nick@deeproove.org>
2012-03-11 18:32:42 -0700

    Fixed parsing test code to handle both cases of brotips.

---
+++ challenges/forensics/200/brotips-scrape.php
@@ -0,0 +1,8 @@
+#!/usr/bin/php
+<?php
+
+/* brotips.com scraper.  Grabs all tips (TWSS) and shoves them in (TWSS)
+ * to a DB4 table.  Tip ID is the key, the tip is the value.  Might also
+ * parse out the number of fists.
+ */
+
