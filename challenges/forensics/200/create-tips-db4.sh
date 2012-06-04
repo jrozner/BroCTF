b9849c7e6b09adfcf70b2569ebba1e39014e84ed
Nick Soracco <nick@deeproove.org>
2012-06-03 19:41:43 -0700

    OK, tips DB4 file should be created with a key correctly, now.

---
+++ challenges/forensics/200/create-tips-db4.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Run all the processes to build this DB4 file.
+#
+
+CSVFILE="brotips.csv"
+DBFILE="brotips.db"
+
+# First up, scrape brotips.com
+./brotips-scrape.php
+
+# Second, check that there's a CSV file with at least 1000 lines
+if [ ! -f "$CSVFILE" ]; then
+	echo "Can't find the brotips file '$CSVFILE' wtf?"
+	exit 1
+elif [ `wc -l "$CSVFILE"` < 1000 ]; then
+	echo "File abormally small."
+	exit 2
+fi
+
+# Build the tips DB.
+./make-tip-db.php
+
+# Insert the key
+./insert-key.php
+
+# Test to see if the key was inserted.
+grep -q "\[key\]" "$DBFILE"
+if [ "0" != "$?" ]; then
+	echo "Crap, no key found, wtf?"
+	exit 3
+fi
+
+# Compress.
+xz "$DBFILE"
+
+# Test
+if [ -e "$DBFILE.xz" ]; then
+	echo "All done."
+else
+	echo "Compression fucked up."
+	exit 4
+fi
