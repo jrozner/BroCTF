5e13c1193bd621b71bdce63ac44324bed396e64d
Nick Soracco <nick@deeproove.org>
2012-06-20 16:15:34 -0700

    misc 100: Challenge complete.

--- challenges/misc/100/generate.sh
+++ challenges/misc/100/generate.sh
@@ -2,6 +2,6 @@
 # 
 # Script to generate the HEX stream used to make the artifact.
 KEY="4b1e1f9654266e821fed5b7468aef468cb83c88c"
-echo "$KEY" | zip -e | ./bin2hex.php | xsel
+echo "$KEY" | zip | xxd -p  | tr -d "\n" | sed -e "s/e/*/g;s/f/#/g" | xsel
 echo "Now, paste your output into Audacity's DTMF generator."
-echo "Give it about half a second for DTMF lenght."
+echo "Give it about half a second for DTMF length."
