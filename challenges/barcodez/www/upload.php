aaa702778161e1e24b2edeb7785cc0602e66f8cb
Joe Rozner <joe@deadbytes.net>
2012-07-09 14:06:20 -0700

    make sure file data is actually present or bail out

--- challenges/barcodez/www/upload.php
+++ challenges/barcodez/www/upload.php
@@ -1,5 +1,8 @@
 <?php
   header('Content-Type: application/json');
+  if (!isset($_POST['file']))
+    die(json_encode(array('msg' => 'File is missing.')));
+
   $decoded = base64_decode($_POST['file']);
   $filename = sha1(time()+mt_rand());
   file_put_contents("/tmp/images/".$filename, $decoded);
