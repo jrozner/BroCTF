8919a24aa0209cfbbf37c5a36a0e7311d5e055ee
Joe Rozner <joe@deadbytes.net>
2012-07-20 01:41:40 -0700

    add value to seeds

--- scorekeeper/db/seeds.js
+++ scorekeeper/db/seeds.js
@@ -3,90 +3,105 @@ module.exports = [
     'title': "Vim Swap",
     'description': "filler1",
     'tier': 1,
-    'flag': "59413f48a4a8586d50ba76966387d40198fd67a8"
+    'flag': "59413f48a4a8586d50ba76966387d40198fd67a8",
+    'value': 100
   },
   {
     'title': "MySQL",
     'description': "filler2",
     'tier': 1,
-    'flag': "cc29882cb5af8efb939ae3719495b36422d6a69a"
+    'flag': "cc29882cb5af8efb939ae3719495b36422d6a69a",
+    'value': 100
   },
   {
     'title': "Brotips",
     'description': "filler3",
     'tier': 1,
-    'flag': "642f0e6de607b1b62a0e979df633340255d2f0eb"
+    'flag': "642f0e6de607b1b62a0e979df633340255d2f0eb",
+    'value': 100
   },
   {
     'title': "DNS TXT",
     'description': "filler4",
     'tier': 1,
-    'flag': "f17f148805505310e276e0d249825d2dc1e280fc"
+    'flag': "f17f148805505310e276e0d249825d2dc1e280fc",
+    'value': 100
   },
   {
     'title': "White Monitor",
     'description': "filler5",
     'tier': 1,
-    'flag': "f41fd980a8b7531d8a97ab594476dbd4486d5c52"
+    'flag': "f41fd980a8b7531d8a97ab594476dbd4486d5c52",
+    'value': 100
   },
   {
     'title': "Multifactor BRAHthentication",
     'description': "filler6",
     'tier': 2,
-    'flag': "fbb0a2fb8ca21c86c099f8dfe2b89c2e74790d31"
+    'flag': "fbb0a2fb8ca21c86c099f8dfe2b89c2e74790d31",
+    'value': 200
   },
   {
     'title': "USB PCAP",
     'description': "filler7",
     'tier': 2,
-    'flag': "5b34b3dedf44d571ac38861e03c8143935c1fe25"
+    'flag': "5b34b3dedf44d571ac38861e03c8143935c1fe25",
+    'value': 200
   },
   {
     'title': "DTMF",
     'description': "filler8",
     'tier': 2,
-    'flag': "4b1e1f9654266e821fed5b7468aef468cb83c88c"
+    'flag': "4b1e1f9654266e821fed5b7468aef468cb83c88c",
+    'value': 200
   },
   {
     'title': "Lulzsec Memorial",
     'description': "filler9",
     'tier': 2,
-    'flag': "af154733cd3c0d9da754e57429084f3ecf138b33"
+    'flag': "af154733cd3c0d9da754e57429084f3ecf138b33",
+    'value': 200
   },
   {
     'title': "Barcodez",
     'description': "filler10",
     'tier': 3,
-    'flag': "b6787d05c8ef26e929c1a16170574455fde69de4"
+    'flag': "b6787d05c8ef26e929c1a16170574455fde69de4",
+    'value': 300
   },
   {
     'title': "Ping Tunnel PCAP of Doom",
     'description': "filler11",
     'tier': 3,
-    'flag': "3889a0a25bc2b15c1652dac688bc84373f17b90d"
+    'flag': "3889a0a25bc2b15c1652dac688bc84373f17b90d",
+    'value': 300
   },
   {
     'title': "Modem",
     'description': "filler12",
     'tier': 3,
-    'flag': "7aca73ebda5e97568e3fd2c1afcf12befd2dacc1"
+    'flag': "7aca73ebda5e97568e3fd2c1afcf12befd2dacc1",
+    'value': 300
   },
   {
-    'title': "Code Golf",
+    'title': "Netcat",
     'description': "filler13",
     'tier': 4,
-    'flag': "a1a93cc630619459340a9f6b27d0ee463b82bc24"
+    'flag': "a1a93cc630619459340a9f6b27d0ee463b82bc24",
+    'value': 400
   },
   {
     'title': "Wavsteg",
     'description': "filler14",
     'tier': 4,
-    'flag': "94e3feb7d129d0cd00a95455024c88b5c5f6b4db"
+    'flag': "94e3feb7d129d0cd00a95455024c88b5c5f6b4db",
+    'value': 400
   },
   {
     'title': "IRDA",
     'description': "filler15",
     'tier': 5,
-    'flag': "2c417a02ffb810176bdaec623181ef3305b3c758"
+    'flag': "2c417a02ffb810176bdaec623181ef3305b3c758",
+    'value': 500
   },
 ]
