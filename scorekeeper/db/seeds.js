1b95a209f2487fec97e9a28a6e2bb8a1d825e4b0
Joe Rozner <joe@deadbytes.net>
2012-07-23 11:14:31 -0700

    seed data for challenges updated

--- scorekeeper/db/seeds.js
+++ scorekeeper/db/seeds.js
@@ -1,105 +1,105 @@
 module.exports = [
   {
     'title': "Vim Swap",
-    'description': "filler1",
+    'description': "All true brogrammers know that IDEs are for scrubs.<br /><a href=\"http://files.broc.tf/1bbbbd2d01f728153d76c576b70f2e6845086fb7\">file</a>",
     'tier': 1,
     'flag': "59413f48a4a8586d50ba76966387d40198fd67a8",
     'value': 100
   },
   {
-    'title': "MySQL",
-    'description': "filler2",
-    'tier': 1,
-    'flag': "cc29882cb5af8efb939ae3719495b36422d6a69a",
-    'value': 100
-  },
-  {
     'title': "Brotips",
-    'description': "filler3",
+    'description': "Here's a bunch of tips for being a better bro.<br /><a href=\"http://files.broc.tf/a7c7e1c333cad50b7a5eb601976da7c9b8d3c02c\">file</a>",
     'tier': 1,
     'flag': "642f0e6de607b1b62a0e979df633340255d2f0eb",
     'value': 100
   },
   {
+    'title': "Lulzsec Memorial",
+    'description': "Sabu was a snitch.<br /><a href=\"http://files.broc.tf/8f47af69db98cbffe0cc43ed167859eecd23f9d1\">file</a>",
+    'tier': 1,
+    'flag': "af154733cd3c0d9da754e57429084f3ecf138b33",
+    'value': 100
+  },
+  {
     'title': "DNS TXT",
-    'description': "filler4",
+    'description': "If you dig(1) around @ some of the servers, you may find something interesting.",
     'tier': 1,
     'flag': "f17f148805505310e276e0d249825d2dc1e280fc",
     'value': 100
   },
   {
     'title': "White Monitor",
-    'description': "filler5",
+    'description': "Security bros need secure monitors to hide the awesome 0days they're writing from all the scrubs at the office.",
     'tier': 1,
     'flag': "f41fd980a8b7531d8a97ab594476dbd4486d5c52",
     'value': 100
   },
   {
     'title': "Multifactor BRAHthentication",
-    'description': "filler6",
+    'description': "Use the Multifactor BRAHthentication token generator to authenticate at: <a href=\"http://brahthentication.broc.tf\">http://brahthentication.broc.tf</a><br /><a href=\"http://files.broc.tf/303eb4628b9cab4dc764f1e026afc5df0e2a8621\">file</a>",
     'tier': 2,
     'flag': "fbb0a2fb8ca21c86c099f8dfe2b89c2e74790d31",
     'value': 200
   },
   {
     'title': "USB PCAP",
-    'description': "filler7",
+    'description': "Is stuxnet in there somewhere?<br /><a href=\"http://files.broc.tf/440709b4ea296baa3b1c3d4e8b51a0163e3ee653\">file</a>",
     'tier': 2,
     'flag': "5b34b3dedf44d571ac38861e03c8143935c1fe25",
     'value': 200
   },
   {
     'title': "DTMF",
-    'description': "filler8",
+    'description': "How fast is your fourier transform?<br /><a href=\"http://files.broc.tf/2041663dc0d9e05afee5aa91765b4b6b7ff5a0de\">file</a>",
     'tier': 2,
     'flag': "4b1e1f9654266e821fed5b7468aef468cb83c88c",
     'value': 200
   },
   {
-    'title': "Lulzsec Memorial",
-    'description': "filler9",
+    'title': "Brocabulator",
+    'description': "Use the bro-translator to help communicate with bros and find the flag at: brocabulator.broc.tf:8008<br /> <a href=\"http://files.broc.tf/\"file</a>",
     'tier': 2,
-    'flag': "af154733cd3c0d9da754e57429084f3ecf138b33",
+    'flag': "cc29882cb5af8efb939ae3719495b36422d6a69a",
     'value': 200
   },
   {
     'title': "Barcodez",
-    'description': "filler10",
+    'description': "Upload barcodez at <a href=\"http://barcodez.broc.tf\">http://barcodez.broc.tf</a> to use the barcode reader at brcodez.broc.tf:7469<br /><a href=\"http://files.broc.tf/2c5ef0198bfc5c3c864115961c4d1fdcb48ee52d>file</a>",
     'tier': 3,
     'flag': "b6787d05c8ef26e929c1a16170574455fde69de4",
     'value': 300
   },
   {
     'title': "Ping Tunnel PCAP of Doom",
-    'description': "filler11",
+    'description': "Maybe Dan Kaminsky can help you with this one if you buy him a jager bomb <a href=\"http://files.broc.tf/b56311e4ee955428e0bb19d331654b8ef781fab9\">file</a>",
     'tier': 3,
     'flag': "3889a0a25bc2b15c1652dac688bc84373f17b90d",
     'value': 300
   },
   {
     'title': "Modem",
-    'description': "filler12",
+    'description': "Where's an acoustic coupler when you need it?<br /><a href=\"http://files.broc.tf/848b1a20414b5cdc31d263775069044e85227f6d\">file</a>",
     'tier': 3,
     'flag': "7aca73ebda5e97568e3fd2c1afcf12befd2dacc1",
     'value': 300
   },
   {
     'title': "Netcat",
-    'description': "filler13",
+    'description': "Check out Netcat running on dual.broc.tf:3863<br /><a href=\"http://files.broc.tf/\">file</a>",
     'tier': 4,
     'flag': "a1a93cc630619459340a9f6b27d0ee463b82bc24",
     'value': 400
   },
   {
     'title': "Wavsteg",
-    'description': "filler14",
+    'description': "Check out this awesome brostep while you search for the flag.<br /><a href=\"http://files.broc.tf/3a569075cf301e48eee66954989f767cbdcb8023\">file</a>",
     'tier': 4,
     'flag': "94e3feb7d129d0cd00a95455024c88b5c5f6b4db",
     'value': 400
   },
   {
     'title': "IRDA",
-    'description': "filler15",
+    'description': "Knock, knock Neo. Follow the black boxes.",
     'tier': 5,
     'flag': "2c417a02ffb810176bdaec623181ef3305b3c758",
     'value': 500
