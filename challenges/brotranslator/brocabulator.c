884a17a2111fb3af4ede29a1e9f008a0132e4721
Joe Rozner <joe@deadbytes.net>
2012-07-23 12:37:40 -0700

    clean up and fixed some shit. Not sure this is correct yet

--- challenges/brotranslator/brocabulator.c
+++ challenges/brotranslator/brocabulator.c
@@ -3,9 +3,6 @@
 // Give it a word, it brocabularizes it, and returns it to you.
 // Has a format string vuln here to allow you to read down the stack.
 //
-// #include <string>
-// #include <iostream>
-// #include <cstdio>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -15,39 +12,55 @@
 #define USER "broseph"
 #define PORT 8008
 #define BROMANCE "/usr/lib/brocabulary/bro"
-#define WORDLEN 129
+#define WORDLEN 300
 
-int translate( int );
+int translate(int);
 
-int main( int argc, char **argv ) {
+int main(int argc, char **argv) {
 
     // Create a socket, daemonize, and then fork() off to
     // the function pointer for translate() as the entry point.
     int socket;
-    daemonize( USER );
+    daemonize(USER);
     if ((socket = initListener(NULL, PORT)) == -1) {
         return 1;
     }
     return startService(&translate, socket);
 }
 
-int translate( int socket ) {
-    // Payload here.
-    char brocabulary[WORDLEN];
+int translate(int socket) {
     int res = 0;
-    volatile char *broskey = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB";
+    FILE *fp;
+    char brocabulary[WORDLEN];
+    char broskey[41];
+
+    if ((fp = fopen("flag.txt", "r")) == NULL) {
+      perror("fopen");
+      close(socket);
+      return 1;
+    }
+
+    if (fgets((char *)&broskey, sizeof(broskey), fp) == NULL) {
+      perror("fgets");
+      close(socket);
+      return 1;
+    }
+
+    fclose(fp);
+
     sendString( socket, "Sup bro?\n" );
-    while( 1 ) {
+    while (1) {
         sendString( socket, "\nWhat's the word?\n>>> " );
-        if (( res = recvUntil( socket, brocabulary, sizeof( brocabulary ) - 1, '\n') ) == 0 ) {
+
+        if ((res = recvUntil(socket, brocabulary, sizeof(brocabulary) - 1, '\n')) == 0) {
             fprintf(stderr, "No datas!\n");
             close(socket);
             return 1;
         }
-        char supstack[100];
-        volatile char *broskey = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-        dprintf( socket, brocabulary ); // NICE. Vulerable output direct to a socket.
+
+        dprintf(socket, brocabulary); // NICE. Vulerable output direct to a socket.
     }
-    close( socket );
+
+    close(socket);
     return 0;
 }
