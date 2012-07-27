1818e514cadf50046ee1e5d6a7c44072526f10ec
Joe Rozner <joe@deadbytes.net>
2012-07-26 22:14:22 -0700

    updated seeds

--- scorekeeper/db/seeds.js
+++ scorekeeper/db/seeds.js
@@ -1,21 +1,21 @@
 module.exports = [
   {
     'title': "Vim Swap",
-    'description': "All true brogrammers know that IDEs are for scrubs.<br /><a href=\"http://files.broc.tf/1bbbbd2d01f728153d76c576b70f2e6845086fb7\">file</a>",
+    'description': "All true brogrammers know that IDEs are for scrubs.<br /><a href=\"http://files.broc.tf/bin/1bbbbd2d01f728153d76c576b70f2e6845086fb7\">file</a>",
     'tier': 1,
     'flag': "59413f48a4a8586d50ba76966387d40198fd67a8",
     'value': 100
   },
   {
     'title': "Brotips",
-    'description': "Here's a bunch of tips for being a better bro.<br /><a href=\"http://files.broc.tf/a7c7e1c333cad50b7a5eb601976da7c9b8d3c02c\">file</a>",
+    'description': "Here's a bunch of tips for being a better bro.<br /><a href=\"http://files.broc.tf/bin/a7c7e1c333cad50b7a5eb601976da7c9b8d3c02c\">file</a>",
     'tier': 1,
     'flag': "642f0e6de607b1b62a0e979df633340255d2f0eb",
     'value': 100
   },
   {
     'title': "Lulzsec Memorial",
-    'description': "Sabu was a snitch.<br /><a href=\"http://files.broc.tf/8f47af69db98cbffe0cc43ed167859eecd23f9d1\">file</a>",
+    'description': "Sabu was a snitch.<br /><a href=\"http://files.broc.tf/bin/8f47af69db98cbffe0cc43ed167859eecd23f9d1\">file</a>",
     'tier': 1,
     'flag': "af154733cd3c0d9da754e57429084f3ecf138b33",
     'value': 100
@@ -36,49 +36,49 @@ module.exports = [
   },
   {
     'title': "Multifactor BRAHthentication",
-    'description': "Use the Multifactor BRAHthentication token generator to authenticate at: <a href=\"http://brahthentication.broc.tf\">http://brahthentication.broc.tf</a><br /><a href=\"http://files.broc.tf/303eb4628b9cab4dc764f1e026afc5df0e2a8621\">file</a>",
+    'description': "Use the Multifactor BRAHthentication token generator to authenticate at: <a href=\"http://brahthentication.broc.tf\">http://brahthentication.broc.tf</a><br /><a href=\"http://files.broc.tf/bin/303eb4628b9cab4dc764f1e026afc5df0e2a8621\">file</a>",
     'tier': 2,
     'flag': "fbb0a2fb8ca21c86c099f8dfe2b89c2e74790d31",
     'value': 200
   },
   {
     'title': "USB PCAP",
-    'description': "Is stuxnet in there somewhere?<br /><a href=\"http://files.broc.tf/440709b4ea296baa3b1c3d4e8b51a0163e3ee653\">file</a>",
+    'description': "Is stuxnet in there somewhere?<br /><a href=\"http://files.broc.tf/bin/440709b4ea296baa3b1c3d4e8b51a0163e3ee653\">file</a>",
     'tier': 2,
     'flag': "5b34b3dedf44d571ac38861e03c8143935c1fe25",
     'value': 200
   },
   {
     'title': "DTMF",
-    'description': "How fast is your fourier transform?<br /><a href=\"http://files.broc.tf/2041663dc0d9e05afee5aa91765b4b6b7ff5a0de\">file</a>",
+    'description': "How fast is your fourier transform?<br /><a href=\"http://files.broc.tf/bin/2041663dc0d9e05afee5aa91765b4b6b7ff5a0de\">file</a>",
     'tier': 2,
     'flag': "4b1e1f9654266e821fed5b7468aef468cb83c88c",
     'value': 200
   },
   {
     'title': "Brocabulator",
-    'description': "Use the bro-translator to help communicate with bros and find the flag at: brocabulator.broc.tf:8008<br /> <a href=\"http://files.broc.tf/\"file</a>",
+    'description': "Use the bro-translator to help communicate with bros and find the flag at: brocabulator.broc.tf:8008<br /> <a href=\"http://files.broc.tf/bin/ee27be5562c41eea1f4ed5a3ddb6e0b413706327\"file</a>",
     'tier': 2,
     'flag': "cc29882cb5af8efb939ae3719495b36422d6a69a",
     'value': 200
   },
   {
     'title': "Barcodez",
-    'description': "Upload barcodez at <a href=\"http://barcodez.broc.tf\">http://barcodez.broc.tf</a> to use the barcode reader at brcodez.broc.tf:7469<br /><a href=\"http://files.broc.tf/2c5ef0198bfc5c3c864115961c4d1fdcb48ee52d>file</a>",
+    'description': "Upload barcodez at <a href=\"http://barcodez.broc.tf\">http://barcodez.broc.tf</a> to use the barcode reader at brcodez.broc.tf:7469<br /><a href=\"http://files.broc.tf/bin/2c5ef0198bfc5c3c864115961c4d1fdcb48ee52d>file</a>",
     'tier': 3,
     'flag': "b6787d05c8ef26e929c1a16170574455fde69de4",
     'value': 300
   },
   {
     'title': "Ping Tunnel PCAP of Doom",
-    'description': "Maybe Dan Kaminsky can help you with this one if you buy him a jager bomb <a href=\"http://files.broc.tf/b56311e4ee955428e0bb19d331654b8ef781fab9\">file</a>",
+    'description': "Maybe Dan Kaminsky can help you with this one if you buy him a jager bomb <a href=\"http://files.broc.tf/bin/b56311e4ee955428e0bb19d331654b8ef781fab9\">file</a>",
     'tier': 3,
     'flag': "3889a0a25bc2b15c1652dac688bc84373f17b90d",
     'value': 300
   },
   {
     'title': "Modem",
-    'description': "Where's an acoustic coupler when you need it?<br /><a href=\"http://files.broc.tf/848b1a20414b5cdc31d263775069044e85227f6d\">file</a>",
+    'description': "Where's an acoustic coupler when you need it?<br /><a href=\"http://files.broc.tf/bin/848b1a20414b5cdc31d263775069044e85227f6d\">file</a>",
     'tier': 3,
     'flag': "7aca73ebda5e97568e3fd2c1afcf12befd2dacc1",
     'value': 300
@@ -92,7 +92,7 @@ module.exports = [
   },
   {
     'title': "Wavsteg",
-    'description': "Check out this awesome brostep while you search for the flag.<br /><a href=\"http://files.broc.tf/3a569075cf301e48eee66954989f767cbdcb8023\">file</a>",
+    'description': "Check out this awesome brostep while you search for the flag.<br /><a href=\"http://files.broc.tf/bin/3a569075cf301e48eee66954989f767cbdcb8023\">file</a>",
     'tier': 4,
     'flag': "94e3feb7d129d0cd00a95455024c88b5c5f6b4db",
     'value': 400
